91ad6588410521b8ad60b5b2b74497ca0c7c2c84
Jérôme Augé <jerome.auge@anakeen.com>
2015-04-15 10:07:06 +0200

    feat(TENGINE_CLIENT_PARAMS): New param validation interface (refs #5465)

--- Actions/tengine_client_params.js
+++ Actions/tengine_client_params.js
@@ -1,43 +1,325 @@
+var TENGINE_CLIENT = TENGINE_CLIENT || {};
+
+TENGINE_CLIENT.paramValidation = (function () {
+    var __CLASS__ = function (UIElmt, callbacks) {
+        this.UIElmt = $(UIElmt);
+        this.callbacks = callbacks;
+    };
+
+    __CLASS__.prototype = {
+        'UIElmt': null,
+        'callbacks': {},
+        'state': null,
+        'started': false,
+        'aborted': false,
+        'info': null,
+        'jqXHR': null,
+        'timer': null,
+
+        'reset': function () {
+            this.state = 'testConnect';
+            this.started = false;
+            this.info = null;
+            this.aborted = false;
+            this.jqXHR = null;
+            this.timer = null;
+            this.clearUIElmt();
+        },
+
+        /**
+         * Display message in current step.
+         * @param msg
+         * @param ok
+         */
+        'display': function (msg, ok) {
+            var elmtId = 'pv-' + this.state;
+            var elmt = document.getElementById(elmtId);
+            var color = "";
+            if (ok === true) {
+                color = "pv-ok";
+            } else if (ok === false) {
+                color = "pv-ko"
+            }
+            if (elmt) {
+                $(elmt).find('.pv-msg').first().text(this.escape(msg));
+                if (color != '') {
+                    $(elmt).find('.pv-msg').addClass(color);
+                    $(elmt).find('.throbber').removeClass('running');
+                } else {
+                    $(elmt).find('.throbber').addClass('running');
+                }
+            } else {
+                alert(msg);
+            }
+        },
+
+        /**
+         * Remove previous messages and ok/ko classes from
+         * the UI element.
+         */
+        'clearUIElmt': function () {
+            $(this.UIElmt).find('.pv-msg').text('').removeClass('pv-ok').removeClass('pv-ko');
+        },
+
+        'log': function (msg) {
+            if (typeof console == 'object') {
+                console.log(msg);
+            }
+        },
+
+        'escape': function (msg) {
+            return $('<div/>').text(msg).html();
+        },
+
+        'getUrl': function (data) {
+            var args = '';
+            for (var key in data) {
+                if (data.hasOwnProperty(key)) {
+                    args = args + '&' + key + '=' + data[key];
+                }
+            }
+            return '?app=TENGINE_CLIENT&action=TENGINE_CLIENT_PARAMS&' + args;
+        },
+
+        /**
+         * Run 'pre' callbacks.
+         */
+        'pre': function () {
+            this.started = true;
+            if (this.callbacks.hasOwnProperty('pre') && typeof this.callbacks['pre'] === 'function') {
+                var args = [];
+                if (this.callbacks.hasOwnProperty('pre-args')) {
+                    args = this.callbacks['pre-args'];
+                }
+                this.callbacks['pre'].apply(this, args);
+            }
+        },
+
+        /**
+         * Run 'post' callbacks.
+         */
+        'post': function () {
+            if (this.callbacks.hasOwnProperty('post') && typeof this.callbacks['post'] === 'function') {
+                var args = [];
+                if (this.callbacks.hasOwnProperty('post-args')) {
+                    args = this.callbacks['post-args'];
+                }
+                this.callbacks['post'].apply(this, args);
+            }
+            this.started = false;
+        },
+
+        /**
+         * Abort execution of param validation.
+         */
+        'abort': function () {
+            this.display('Abort requested', false);
+            this.aborted = true;
+            if (this.timer) {
+                window.clearTimeout(this.timer);
+            }
+            this.jqXHR.abort();
+            this.post();
+        },
+
+        'isRunning': function () {
+            return this.started;
+        },
+
+        /**
+         * Main entry point for starting the param validation.
+         * @param delay
+         * @returns {*}
+         */
+        'run': function (delay) {
+            if (this.aborted) {
+                return;
+            }
+            if (!this.started) {
+                this.reset();
+                this.pre();
+            }
+            if (typeof delay == 'number') {
+                var _this = this;
+                this.timer = window.setTimeout(function () {
+                    _this.run();
+                }, delay);
+                return;
+            }
+            switch (this.state) {
+                case 'testConnect':
+                    return this.testConnect();
+                    break;
+                case 'newTask':
+                    return this.newTask();
+                    break;
+                case 'waitForTaskDone':
+                    return this.waitForTaskDone();
+                    break;
+                case 'waitForCallback':
+                    return this.waitForCallback();
+                    break;
+                case 'clearTask':
+                    return this.clearTask();
+                    break;
+                case null:
+                    if (this.callbacks.hasOwnProperty('post')) {
+                        this.callbacks.post(this);
+                    }
+                    return this.post();
+            }
+            this.display("Unknown state '" + this.state + "'.", false);
+        },
+
+        /**
+         * jQuery().ajax() wrapper
+         * @param args
+         * @param callbacks
+         */
+        'ajax': function (args, callbacks) {
+            if (typeof callbacks != 'object') {
+                callbacks = {};
+            }
+            var opts = {
+                'url': this.getUrl(args),
+                'context': this,
+                'success': function (data, textStatus, jqXHR) {
+                    if (typeof data != 'object') {
+                        this.display("Error: data is not an object.", false);
+                        return this.post();
+                    }
+                    if (!data.hasOwnProperty('success')) {
+                        this.display("Error: missing 'success' property on data object.", false);
+                        return this.post();
+                    }
+                    if (!data.success) {
+                        if (callbacks.hasOwnProperty('error') && typeof callbacks['error'] === 'function') {
+                            callbacks['error'].apply(this, [data, textStatus, jqXHR]);
+                        } else {
+                            this.display(data.message, false);
+                        }
+                        return this.post();
+                    }
+                    if (data.hasOwnProperty('next')) {
+                        var delay = 2000;
+                        if (this.state != data.next) {
+                            delay = null;
+                            this.display(data.message, true);
+                        } else {
+                            this.display(data.message);
+                        }
+                        if (callbacks.hasOwnProperty('success') && typeof callbacks['success'] === 'function') {
+                            callbacks['success'].apply(this, [data, textStatus, jqXHR]);
+                        }
+                        this.state = data.next;
+                        return this.run(delay);
+                    }
+                    if (callbacks.hasOwnProperty('success') && typeof callbacks['success'] === 'function') {
+                        callbacks['success'].apply(this, [data, textStatus, jqXHR]);
+                    }
+                    return this.post();
+                },
+                'error': function (jqXHR, textStatus, errorThrown) {
+                    if (callbacks.hasOwnProperty('error') && typeof callbacks['error'] === 'function') {
+                        callbacks['error'].apply(this, [null, textStatus, jqXHR]);
+                    } else {
+                        this.display("Error: " + textStatus, false);
+                    }
+                    return this.post();
+                }
+            };
+            this.display('');
+            this.jqXHR = $.ajax(opts);
+        },
+
+        'testConnect': function () {
+            this.log("Running testConnect...");
+            this.ajax({
+                'validate': 'testConnect'
+            });
+        },
+
+        'newTask': function () {
+            this.log("Running newTask...");
+            this.ajax(
+                {
+                    'validate': 'newTask'
+                }, {
+                    'success': function (data, textStatus, jqXHR) {
+                        this.info = data.info;
+                    }
+                }
+            );
+        },
+
+        'waitForTaskDone': function () {
+            this.log("Runnning waitForTaskDone...");
+            this.ajax({
+                'validate': 'waitForTaskDone',
+                'tid': this.info.tid
+            });
+        },
+
+        'waitForCallback': function () {
+            this.log("Running waitForCallback...");
+            this.ajax({
+                'validate': 'waitForCallback',
+                'tid': this.info.tid
+            });
+        },
+
+        'clearTask': function () {
+            this.log("Running clearTask...");
+            this.ajax({
+                'validate': 'clearTask',
+                'tid': this.info.tid
+            });
+        }
+    };
+
+    return __CLASS__;
+})();
+
 $(document).ready(function () {
-    
+
     $('input, .ui-button').button();
     var buttons = "";
     $("select").each(function () {
         var rb = '', idrb = '';
         for (var i = 0; i < this.options.length; i++) {
             idrb = this.id + '_' + i;
-            rb = $('<input type="radio" value="'+this.options[i].value+'" id="' + idrb + '" ' + ((this.options[i].selected) ? "checked" : "") + '  name="' + this.name + '" /><label for="' + idrb + '">' + this.options[i].label + '</label>');
+            rb = $('<input type="radio" value="' + this.options[i].value + '" id="' + idrb + '" ' + ((this.options[i].selected) ? "checked" : "") + '  name="' + this.name + '" /><label for="' + idrb + '">' + this.options[i].label + '</label>');
             $(this).parent().append(rb);
         }
-	
-        rb=$('<input type="hidden" data-original="1" id="'+this.id+'"/>');
+
+        rb = $('<input type="hidden" data-original="1" id="' + this.id + '"/>');
         $(this).parent().append(rb);
         $(this).remove();
     });
-    
+
     $('input[type=radio]').parent().buttonset();
-    
+
     $("body").on("MODPARAMETER", function (event, data) {
         if (data.success) {
             var pid = data.parameterid;
             if (data.data.modify) {
-                $('#' + data.data.parameterid +',#'+data.data.parameterid+'_0').parents('.editapplicationparameter').find('div[data-type=label] label').addClass('modified');
-		
+                $('#' + data.data.parameterid + ',#' + data.data.parameterid + '_0').parents('.editapplicationparameter').find('div[data-type=label] label').addClass('modified');
+
             }
         } else {
             alert(data.error);
         }
     });
-    
-    $("body").on("mouseup", "label.ui-button",function (event) {
-        if (! $(this).hasClass('ui-state-active')) {
-            var originValue=$('#'+$(this).attr('for')).val();
+
+    $("body").on("mouseup", "label.ui-button", function (event) {
+        if (!$(this).hasClass('ui-state-active')) {
+            var originValue = $('#' + $(this).attr('for')).val();
             $(this).parents('form').find('input[data-original=1]').val(originValue);
             sendParameterApplicationData(this);
         }
     });
-    
-    $("form").on("keypress", "input[type=text]",function (e) {
+
+    $("form").on("keypress", "input[type=text]", function (e) {
         var code = e.keyCode || e.which;
         if (code == 13) {
             e.preventDefault();
@@ -46,41 +328,30 @@ $(document).ready(function () {
         }
     });
 
-
-    $('#connectionstatus').hide();
-
-    $('#checkconnection').on('click', function() {
-	$("#checkconnection").attr("disabled", true).addClass("ui-state-disabled");
-	$('#checkconnection i').show();
-	$('#connectionstatus').hide();
-	$('#connectionstatus .infos').hide();
-	$('#errorMessage').hide(); 
-	$.ajax({
-	    url: '?app=TENGINE_CLIENT&action=TENGINE_CLIENT_INFOS',
-	    type: "POST",
-	    success: function(data) {
-		$("#checkconnection").attr("disabled", false).removeClass("ui-state-disabled");
-		$('#checkconnection i').hide();
-		if (data.success) {
-		    $('#TESVersion .val').html(data.info.version).show();
-		    $('#TESBuild .val').html(data.info.release).show();
-		    $('#TESMaxClient .val').html(data.info.max_client);
-		    $('#connectionstatus .status').html('OK').removeClass('ko').addClass('ok');
-		    $('#connectionstatus .infos').show();
-		} else {
-		    $('#connectionstatus .status').html('ERROR').removeClass('ok').addClass('ko');
-		    $('#errorMessage').html(data.message).show(); 
-		}
-		$('#connectionstatus').show();
-	    },
-	    error: function() { 
-		$("#checkconnection").attr("disabled", false).removeClass("ui-state-disabled");
-		$('#checkconnection i').hide();
-		$('#errorMessage').html('Ooops, something is wrong...').show(); 
-		$('#connectionstatus .status').html('ERROR').removeClass('ok').addClass('ko');
-		$('#connectionstatus').show();
-	    }
-	});
+    $('#checkconnection').on('click', function () {
+        if (window.paramValidation && window.paramValidation.isRunning()) {
+            window.paramValidation.abort();
+        } else {
+            var button = this;
+            window.paramValidation = new TENGINE_CLIENT.paramValidation(
+                document.getElementById('param-validation'),
+                {
+                    'pre': function (button) {
+                        if (!this.hasOwnProperty('_ui_button_text')) {
+                            this._ui_button_text = $(button).find('.ui-button-text').first().text();
+                        }
+                        $(button).find('.ui-button-text').first().text('⬛ Stop');
+                    },
+                    'pre-args': [button],
+                    'post': function (button) {
+                        if (this.hasOwnProperty('_ui_button_text')) {
+                            $(button).find('.ui-button-text').first().text(this._ui_button_text);
+                        }
+                    },
+                    'post-args': [button]
+                }
+            );
+            window.paramValidation.run();
+        }
     });
-    
 });
--- Actions/tengine_client_params.php
+++ Actions/tengine_client_params.php
@@ -9,6 +9,16 @@ require_once "FDL/editutil.php";
 
 function tengine_client_params(Action & $action)
 {
+    $validate = getHttpVars('validate', null);
+    if ($validate === null) {
+        tengine_client_params_ui($action);
+    } else {
+        tengine_client_params_validate($action);
+    }
+}
+
+function tengine_client_params_ui(Action & $action)
+{
     $action->lay->eSet('HTML_LANG', str_replace('_', '-', getParam('CORE_LANG', 'fr_FR')));
     
     editmode($action);
@@ -17,4 +27,214 @@ function tengine_client_params(Action & $action)
     $action->parent->AddJsRef("TENGINE_CLIENT:tengine_client.js", true);
     $action->parent->AddJsRef("TENGINE_CLIENT:tengine_client_params.js", true);
     $action->parent->AddCssRef("TENGINE_CLIENT:tengine_client.css");
+}
+
+function tengine_client_params_validate(Action & $action)
+{
+    require_once "FDL/editutil.php";
+    $validate = getHttpVars('validate', '');
+    
+    switch ($validate) {
+        case 'testConnect':
+            testConnect($action);
+            break;
+
+        case 'newTask':
+            newTask($action);
+            break;
+
+        case 'waitForTaskDone':
+            waitForTaskDone($action);
+            break;
+
+        case 'taskCallback':
+            taskCallback($action);
+            break;
+
+        case 'waitForCallback':
+            waitForCallback($action);
+            break;
+
+        case 'clearTask':
+            clearTask($action);
+            break;
+    }
+    error(sprintf("Unknown state '%s'.", $validate));
+}
+/**
+ * @param Action $action
+ * @return \Dcp\TransformationEngine\Client
+ */
+function newTeClient(Action & $action)
+{
+    return new \Dcp\TransformationEngine\Client($action->getParam("TE_HOST") , $action->getParam("TE_PORT"));
+}
+/**
+ * @param $response
+ */
+function reply($response)
+{
+    header('Content-Type: application/json');
+    echo json_encode($response);
+    exit;
+}
+/**
+ * @param string $message
+ * @param null $info
+ * @param null $next
+ */
+function success($message = '', $info = null, $next = null)
+{
+    $response = array(
+        'success' => true,
+        'message' => $message,
+        'info' => $info,
+        'next' => $next
+    );
+    reply($response);
+}
+/**
+ * @param string $message
+ * @param null $info
+ * @param null $next
+ */
+function error($message = '', $info = null, $next = null)
+{
+    $response = array(
+        'success' => false,
+        'message' => $message,
+        'info' => $info,
+        'next' => $next
+    );
+    reply($response);
+}
+/**
+ * @param Action $action
+ */
+function testConnect(Action & $action)
+{
+    $info = array();
+    $err = \Dcp\TransformationEngine\Manager::checkParameters();
+    if ($err != '') {
+        error($err);
+    } else {
+        $te = newTeClient($action);
+        $err = $te->retrieveServerInfo($info, true);
+        if ($err != '') {
+            error($err);
+        }
+    }
+    success(sprintf(_("tengine_client:Successfully connected to server (version %s release %s)") , $info['version'], $info['release']) , $info, 'newTask');
+}
+/**
+ * @param Action $action
+ */
+function newTask(Action & $action)
+{
+    include_once 'FDL/Lib.Vault.php';
+    
+    $te = newTeClient($action);
+    $info = array();
+    $te_name = 'utf8';
+    $fkey = '';
+    $tmpFile = tempnam(getTmpDir() , '');
+    if ($tmpFile === false) {
+        error(_("tengine_client:Could not create temporary file."));
+    }
+    if (file_put_contents($tmpFile, 'hello world.') === false) {
+        error(sprintf(_("tengine_client:Error writing content to temporary file '%s'") , $tmpFile));
+    }
+    $appName = $action->parent->name;
+    $actionName = $action->name;
+    $urlindex = getOpenTeUrl(array(
+        'app' => $appName,
+        'action' => $actionName,
+        'validate' => 'taskCallback'
+    ));
+    $callback = sprintf("%s&app=%s&action=%s&validate=taskCallback", $urlindex, $appName, $actionName);
+    
+    $err = $te->sendTransformation($te_name, $fkey, $tmpFile, $callback, $info);
+    if ($err != '') {
+        unlink($tmpFile);
+        error($err);
+    }
+    unlink($tmpFile);
+    success(sprintf(_("tengine_client:Created new task with tid '%s'.") , $info['tid']) , $info, 'waitForTaskDone');
+}
+/**
+ * @param Action $action
+ */
+function waitForTaskDone(Action & $action)
+{
+    $tid = getHttpVars('tid', '');
+    if ($tid == '') {
+        error(sprintf(_("tengine_client:Missing tid argument.")));
+    }
+    $te = newTeClient($action);
+    $info = array();
+    $err = $te->getInfo($tid, $info);
+    if ($err != '') {
+        error($err);
+    }
+    switch ($info['status']) {
+        case \Dcp\TransformationEngine\Client::TASK_STATE_ERROR:
+            error(sprintf(_("tengine_client:Task failed (status '%s').") , $info['status']) , $info);
+            break;
+
+        case \Dcp\TransformationEngine\Client::TASK_STATE_INTERRUPTED:
+            error(sprintf(_("tengine_client:Task was interrupted (status '%s').") , $info['status']) , $info);
+            break;
+
+        case \Dcp\TransformationEngine\Client::TASK_STATE_SUCCESS:
+            success(sprintf(_("tengine_client:Task completed (status '%s').") , $info['status']) , $info, 'waitForCallback');
+    }
+    success(sprintf(_("tengine_client:Pending task (status '%s').") , $info['status']) , $info, 'waitForTaskDone');
+}
+/**
+ * @param Action $action
+ */
+function taskCallback(Action & $action)
+{
+    print 'CALLBACK:OK';
+    exit;
+}
+/**
+ * @param Action $action
+ */
+function waitForCallback(Action & $action)
+{
+    $tid = getHttpVars('tid', '');
+    if ($tid == '') {
+        error(sprintf(_("tengine_client:Missing tid argument.")));
+    }
+    $te = newTeClient($action);
+    $info = array();
+    $err = $te->getInfo($tid, $info);
+    if ($err != '') {
+        error($err);
+    }
+    if (isset($info['callreturn']) && $info['callreturn'] != '') {
+        if ($info['callreturn'] == 'CALLBACK:OK') {
+            success(sprintf(_("tengine_client:Callback success.")) , $info, 'clearTask');
+        } else {
+            error(sprintf(_("tengine_client:Callback failed with message: %s") , $info['callreturn']));
+        }
+    }
+    success(sprintf(_("tengine_client:Task's status is '%s' with comment '%s'.") , $info['status'], $info['comment']) , $info, 'waitForCallback');
+}
+/**
+ * @param Action $action
+ */
+function clearTask(Action & $action)
+{
+    $tid = getHttpVars('tid', '');
+    if ($tid == '') {
+        error(sprintf(_("tengine_client:Missing tid argument.")));
+    }
+    $te = newTeClient($action);
+    $err = $te->abortTransformation($tid);
+    if ($err != '') {
+        error($err);
+    }
+    success(sprintf(_("tengine_client:Cleared task with tid '%s'.") , $tid));
 }
